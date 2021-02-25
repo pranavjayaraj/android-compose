dab9dd6bbaee2bcee3d04a494e71a469bcc36305
pranavjayaraj7@gmail.com <pranavjayaraj7@gmail.com>
2021-02-25 12:24:27 +0530

    fixed imports

--- app/src/main/java/com/example/androiddevchallenge/components/puppies.kt
+++ app/src/main/java/com/example/androiddevchallenge/components/puppies.kt
@@ -1,13 +1,17 @@
 package com.example.androiddevchallenge.components
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
